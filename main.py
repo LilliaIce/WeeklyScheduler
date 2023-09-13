9cce4a2918093c6d29f64acd1826e1d75006e5c2
Lillia <lillia.ice@g.austincc.edu>
2023-09-13 10:02:20 -0500

    Major overhaul of main, implementing code organizational practices, such as organizing long parts of main into functions and establishing constants rather than using literals. Also moved functions around; there's enough of them that I need to figure out how to make the function section less daunting to look through. I also removed the menu.py file and Menu class; I'll likely add them back later when I understand how to better implement them.

--- main.py
+++ main.py
@@ -10,48 +10,61 @@ a task as repeatable each week.
 
 import os
 from week import Week
-from menu import Menu
+from project import Project
 from day import Day
 from file import File
 
 # list of the days of the week
 DAYSOFWEEK = ('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Master')
 
+# prompt for main menu
+MAINP = " > "
+# prompt for day menu
+DAYP = "\t > "
+# prompt for options
+OPTIONP = "\t\t > "
+
+# menus
+MAINMENU = ("Enter 'q' or nothing to quit the program.")
+DAYMENU = ("Enter 'a' to add a task, 'd' to delete a task, 'r' to "
+            "rename a task, 'm' to move a task,  or 't' to toggle a task.")
+
+
 def addTask(day):
-    i = input(Menu.OPTIONP)
+    i = input(OPTIONP)
     while i != 'q' and not i.isspace():
         day.addTask(i)
-        i = input(Menu.OPTIONP)
+        i = input(OPTIONP)
 
 
 def removeTask(day):
-    i = input(Menu.OPTIONP)
+    i = input(OPTIONP)
     while i != 'q' and not i.isspace():
         try:
             day.removeTask(i)
         except:
             print("Sorry, that wasn't a valid task.")
-        i = input(Menu.OPTIONP)
+        i = input(OPTIONP)
 
 
 def moveTask(day, schedule):
-    i = input(Menu.OPTIONP)
+    i = input(OPTIONP)
     while i != 'q' and not i.isspace():
-        j = input(Menu.OPTIONP).title().strip()
+        j = input(OPTIONP).title().strip()
         if j != 'Q' and not j.isspace():
             j = schedule.getDay(j)
             day.moveTask(i, j)
-        i = input(Menu.OPTIONP)
+        i = input(OPTIONP)
 
 
 def toggleTask(day):
-    i = input(Menu.OPTIONP)
+    i = input(OPTIONP)
     while i != 'q' and not i.isspace():
         try:
             day.toggleTask(i)
         except:
             pass
-        i = input(Menu.OPTIONP)
+        i = input(OPTIONP)
 
 
 def createWeek(days):
@@ -63,23 +76,17 @@ def createWeek(days):
     return Week(items)
 
 
-def displayTasks(tasks):
-    """Displays the tasks for each in a separate column that extends
-    over the longest task list"""
-    for item in DAYSOFWEEK:
-        print(f"{item:30}", end="")
-    print()
-    longest = max([len(i.getTasks()) for i in tasks])
-    for position in range(0, longest):
-        for i in tasks:
-            try:
-                print(f"{i.getTasks()[position]:30}", end="")
-            except:
-                print(f"{'':30}", end="")
-        print()
+def addProject():
+    i = input(OPTIONP)
+    if i != 'q' and not i.isspace():
+        day.addProject(i)
 
 
-def getWeeksTasks(schedule):
+def removeProject():
+    pass
+
+
+def getWeekTasks(schedule):
     """"""
     list = []
     for item in schedule.getDays():
@@ -87,12 +94,94 @@ def getWeeksTasks(schedule):
     return list
 
 
-def main():
-    # will read file if able to; if not able, will print line to
-    # warn user
-    kill = input("Press any key to start: ")
-    if kill == 'd':
-        File.clearFile()
+def getColumnWidth(week):
+    """ Returns the width of the longest item plus CONST"""
+    list = []
+    CONST = 5
+    for day in week:
+        for item in day.getTasks():
+            list.append(item)
+    return max(len(i) for i in list) + CONST
+
+
+def getLongestList(tasks):
+    """ Returns the length of the longest task list"""
+    return max(len(i.getTasks()) for i in tasks)
+
+
+def displayWeek(week):
+    """ Displays the tasks for each day in a separate column that extends
+    through the longest task list 
+    
+    The column's width is generated to fit the longest task with some padding
+    each time """
+    width = getColumnWidth(week)
+    maxRow = getLongestList(week)
+
+    for i in DAYSOFWEEK:
+        print(f"{i:{width}}", end="")
+    print()
+
+    for row in range(0, maxRow):
+        for day in week:
+            try:
+                print(f"{day.getTasks()[row]:{width}}", end="")
+            except:
+                print(f"{'':{width}}", end="")
+        print()
+
+
+def displayDay(day):
+    """ Displays a day's tasks """
+    width = getColumnWidth(day)
+    maxRow = getLongestList(day)
+
+    print(day)
+
+    for row in range(0, maxRow):
+        print(f"{day.getTasks()[row]:{width}}", end="")
+    print()
+
+
+def displayTasks():
+    pass
+
+
+def dayLoop(schedule, dayInput):
+        ADD = 'A'
+        DELETE = 'D'
+        RENAME = 'R'
+        MOVE = 'M'
+        TOGGLE = 'T'
+
+        day = schedule.getDay(dayInput)
+        i = "none"
+        while i != 'q' and not i.isspace():
+            # gets the user's input for the day menu
+            print(DAYMENU)
+            i = input(DAYP)
+
+            if i.upper() == ADD:
+                addTask(day)
+            elif i.upper() == DELETE:
+                removeTask(day)
+            elif i.upper() == RENAME:
+                pass
+            elif i.upper() == MOVE:
+                moveTask(day, schedule)
+            elif i.upper() == TOGGLE:
+                toggleTask(day)
+            else:
+                # if input doesn't correspond to any of the available
+                # options, user will be prompted for a new input
+                print()
+                print(f"{i} isn't a valid option.")
+            File.saveFile(schedule)
+
+
+def startup():
+    """ Function will read file if able to; if unable to, function will 
+    print line to warn user """
     try:
         schedule = File.readFile()
     except:
@@ -100,14 +189,19 @@ def main():
               "replaced when the program creates a new file, ",
               "immediately exit the program.")
         print("Press 'Enter' to continue.")
-        input("")
+        input()
         schedule = createWeek(DAYSOFWEEK)
         print("Data has been replaced.")
+    return schedule
+
+
+def main():
+    schedule = startup()
     print()
-    print(Menu.MAINMENU)
+    print(MAINMENU)
 
     print()
-    displayTasks(getWeeksTasks(schedule))
+    displayWeek(getWeekTasks(schedule))
     print()
 
     dayInput = "none"
@@ -117,50 +211,18 @@ def main():
         print(*schedule.getDays(), sep = ", ")
         print()
 
-        dayInput = input("Please select the day. > ").title().strip()
+        dayInput = input(DAYP).title().strip()
 
         while dayInput not in DAYSOFWEEK:
             print("That's not a day of the week. Please choose a day from Monday to Friday " +
                   "or the 'master' list.")
-            dayInput = input(" > ").title().strip()
+            dayInput = input(MAINP).title().strip()
 
-        day = schedule.getDay(dayInput)
-
-        i = "none"
-        while i != 'q' and not i.isspace():
-            # gets the user's input for the day menu
-            print(Menu.DAYMENU)
-            i = input(Menu.DAYP)
-
-            if i == 'a':
-                # add tasks
-                addTask(day)
-            elif i == 'd':
-                # remove tasks
-                removeTask(day)
-            elif i == 'r':
-                # rename tasks
-                pass
-            elif i == 'm':
-                # move tasks to another day
-                moveTask(day, schedule)
-            
-            elif i == 't':
-                # toggle tasks
-                toggleTask(day)
-            else:
-                # if input doesn't correspond to any of the available
-                # options, user will be prompted for a new input
-                print()
-                print(f"{i} isn't a valid option.")
-            # when the user finishes with the above options, the program
-            # will save to the file and display the main menu again
-            File.saveFile(schedule)
+        dayLoop(schedule, dayInput)
         print()
         os.system("cls")
-        displayTasks(getWeeksTasks(schedule))
+        displayWeek(getWeekTasks(schedule))
         print()
-
 
 if __name__ == '__main__':
     main()
